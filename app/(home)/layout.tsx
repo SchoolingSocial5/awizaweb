d572c43a39865d7d3a4171043361febbef71c4d2
SchoolingSocial5 <schoolingsocial5@gmail.com>
2026-03-28 08:50:24 +0100

    Removed loader

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -14,7 +14,7 @@ import UserAlert from '@/components/Messages/UserAlert'
 import Link from 'next/link'
 import PageLoader from '@/components/Public/PageLoader'
 import ProductStore from '@/src/zustand/Product'
-import BlogStore from '@/src/zustand/Blog'
+// import BlogStore from '@/src/zustand/Blog'
 
 export default function RootLayout({
   children,
@@ -26,7 +26,7 @@ export default function RootLayout({
   const lastScrollY = useRef(0)
   const isOutOfView = useRef(false)
   const { cartProducts } = ProductStore()
-  const { blogs } = BlogStore()
+  // const { blogs } = BlogStore()
 
   // const [isMd, setIsMd] = useState(false)
   const pathname = usePathname()
@@ -76,7 +76,7 @@ export default function RootLayout({
       <UserAlert />
       <PublicHeader />
       <PublicNavbar />
-      <div className={`${blogs.length > 0 ? "h-auto" : "h-[100vh] overflow-hidden"} text-[var(--dark)] bg-white`}> {children}</div>
+      {/* <div className={`${blogs.length > 0 ? "h-auto" : "h-[100vh] overflow-hidden"} text-[var(--dark)] bg-white`}> {children}</div> */}
       {cartProducts.length > 0 && (
         <Link
           href={'/check-out'}
